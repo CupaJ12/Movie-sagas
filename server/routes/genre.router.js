8e6e173879e8d5c7ad1665838bd1aa7b7dd8d6ca
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2023-02-16 20:57:55 -0500

    got the router working!

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,42 +4,36 @@ const pool = require('../modules/pool');
 
 router.get('/:id', (req, res) => {
 	// Add query to get all genres
-  console.log('1', req.params.action.payload);
-  console.log('2', action.payload);
-  console.log('3', req.params);
-  console.log('4', req.params.id);
-
-  let queryText = `SELECT “genres”.“name” FROM “movies”
-  JOIN “movies_genres” ON “movies_genres”.“movie_id” = “movies”.“id”
-  JOIN “genres” ON “genres”.“id” = “movies_genres”.“genre_id”
-  WHERE “movies”.“id” = ‘${req.params.id}’;`;
-  pool.query(queryText)
-  .then((response) => {
-    console.log('response.rows:', response.rows);
-    res.send(response.rows);
-  }).catch((error) => {
-    console.log('error in genre.router GET request', error);
-    res.sendStatus(500);
-  });
-
-
-
-  
+	
+	let queryText = `SELECT "genres"."name" FROM "movies"
+	JOIN "movies_genres" ON "movies_genres"."movie_id" = "movies"."id"
+	JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
+	WHERE "movies"."id" = ${req.params.id};`;
+	pool
+		.query(queryText)
+		.then((response) => {
+			console.log('response.rows:', response.rows);
+			res.send(response.rows);
+		})
+		.catch((error) => {
+			console.log('error in genre.router GET request', error);
+			res.sendStatus(500);
+		});
 
 	// let queryText = `SELECT "genres", "name", "movies"."title" FROM "movies"
-//   JOIN "movies_genres" ON "movies_genres"."movies_id" = "movies"."id"
-//   JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
-//   WHERE "movies"."title" = '${req.params.id}';`;
-// 	pool
-// 		.query(queryText)
-// 		.then((res) => {
-// 			console.log(res.rows);
-// 			res.send(res.rows);
-// 		})
-// 		.catch((err) => {
-// 			console.log('Error completing SELECT genres query', err);
-// 			res.sendStatus(500);
-// 		});
+	//   JOIN "movies_genres" ON "movies_genres"."movies_id" = "movies"."id"
+	//   JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
+	//   WHERE "movies"."title" = '${req.params.id}';`;
+	// 	pool
+	// 		.query(queryText)
+	// 		.then((res) => {
+	// 			console.log(res.rows);
+	// 			res.send(res.rows);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log('Error completing SELECT genres query', err);
+	// 			res.sendStatus(500);
+	// 		});
 });
 
 module.exports = router;
